833c3134e8fdc901ad42dbefd8b3af67909ec526
InfernalWolf08 <jayinjoshi@gmail.com>
2025-07-10 20:08:40 -0700

    A* Pain

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -6,11 +6,12 @@ public class MonsterController : MonoBehaviour
 {
     [Header("Stats")]
     public float health=100;
-    [Range(0.1f, 10f)] public float speed;
+    [Range(0.1f, 5f)] public float speed;
     private float baseSpeed;
 
     [Header("AI")]
     public Transform player;
+    public AIPath pathing;
     public AIDestinationSetter AiDest;
 
     void Start()
@@ -19,10 +20,12 @@ public class MonsterController : MonoBehaviour
         player = GameObject.FindWithTag("Player").transform;
         baseSpeed = speed;
         AiDest.target = player;
+        pathing.maxSpeed = speed;
     }
 
     void Update()
     {
-
+        // Flip towards player
+        transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().flipX = player.transform.position.x<transform.position.x;
     }
 }
